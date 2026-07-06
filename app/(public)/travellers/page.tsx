a1e2d6f80ccfb13b1dbe7ecaaa072ad64da84fca
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-07-06 18:23:24 +0300

    initial travellers setup

--- app/(public)/travellers/page.tsx
+++ app/(public)/travellers/page.tsx
@@ -1,3 +1,22 @@
-export default function StoriesPage() {
-  return <main>TravellersPage</main>;
+import type { Metadata } from 'next';
+import PageTitle from '@/components/PageTitle/PageTitle';
+import TravellersList from '@/components/TravellersList/TravellersList';
+import css from './TravellersPage.module.css';
+
+export const metadata: Metadata = {
+  title: 'Мандрівники',
+  description: 'Список зареєстрованих мандрівників',
+};
+
+export default function TravellersPage() {
+  return (
+    <main className={css.page}>
+      <section className={css.section}>
+        <div className={css.container}>
+          <PageTitle>Мандрівники</PageTitle>
+          <TravellersList />
+        </div>
+      </section>
+    </main>
+  );
 }
