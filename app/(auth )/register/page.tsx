83a67fd9d5b37f7eea659d157448e3cf7c1ee223
Anastasia Baranenko <anastasiyabaranenko78@gmail.com>
2026-07-08 19:36:44 +0300

    add register page with auth nav, footer, header and form

--- app/(auth )/register/page.tsx
+++ app/(auth )/register/page.tsx
@@ -1,4 +1,4 @@
-// import AuthHeader from '@/components/AuthHeader/AuthHeader';
+import AuthHeader from '@/components/AuthHeader/AuthHeader';
 import MainAuthNav from '@/components/MainAuthNav/MainAuthNav';
 import AuthFooter from '@/components/AuthFooter/AuthFooter';
 import RegistrationForm from '@/components/RegistrationForm/RegistrationForm';
@@ -8,7 +8,7 @@ export default function RegisterPage() {
   return (
     <main className={styles.registerPageContainer}>
       <div className={styles.container}>
-        {/* <AuthHeader /> */}
+        <AuthHeader />
         <MainAuthNav />
         <RegistrationForm />
         <AuthFooter />
