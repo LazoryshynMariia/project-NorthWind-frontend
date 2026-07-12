12ed6f077819a33dffeba7b8aa75bc579552cdb6
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2026-07-12 15:45:36 +0300

    Fix Logo component

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -11,13 +11,6 @@ const Logo = () => {
     >
       <span className={styles.icon} aria-hidden="true">
         <svg width="28" height="28" viewBox="0 0 24 24" fill="none">
-          <circle
-            cx="12"
-            cy="12"
-            r="11"
-            stroke="currentColor"
-            strokeWidth="1.5"
-          />
           <path
             d="M7 15c0-4 3-7 9-7 0 6-3 9-7 9-1 0-2 0-2-2Z"
             stroke="currentColor"
