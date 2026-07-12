c0d9c587eff4b1130a2ef85e67a2462082db75d3
Marina <marina.miram@gmail.com>
2026-07-12 10:12:18 +0300

    .

---
+++ components/UserBar/UserBar.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { RxExit } from 'react-icons/rx';
+
+import { ConfirmModal } from '@/components/ConfirmModal/ConfirmModal';
+
+import css from './UserBar.module.css';
+
+interface User {
+  name: string;
+  avatarUrl?: string;
+}
+
+interface UserBarProps {
+  user: User;
+  onLogout: () => Promise<void> | void;
+}
+
+export const UserBar = ({ user, onLogout }: UserBarProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      await onLogout();
+    } finally {
+      setIsModalOpen(false);
+    }
+  };
+
+  return (
+    <div className={css.userBar}>
+      <Image
+        src={user.avatarUrl || '/default-avatar.png'}
+        alt={
+          user.name
+            ? `Аватар користувача ${user.name}`
+            : 'Аватар користувача'
+        }
+        width={32}
+        height={32}
+        className={css.avatar}
+      />
+
+      <span className={css.userName}>
+        {user.name || "Ім'я"}
+      </span>
+
+      <span className={css.divider} />
+
+      <button
+        type="button"
+        className={css.logoutButton}
+        aria-label="Вийти з системи"
+        onClick={() => setIsModalOpen(true)}
+      >
+        <RxExit size={20} />
+      </button>
+
+      <ConfirmModal
+        isOpen={isModalOpen}
+        title="Ви точно хочете вийти?"
+        confirmButtonText="Вийти"
+        cancelButtonText="Відмінити"
+        onConfirm={handleLogout}
+        onCancel={() => setIsModalOpen(false)}
+      >
+        Ми будемо сумувати за вами!
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default UserBar;
