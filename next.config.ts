9984d8612f523ff8cf1f3aefac53618a3642e125
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-07-12 20:07:02 +0300

    update pagination styles and image config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ftp.goit.study',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
